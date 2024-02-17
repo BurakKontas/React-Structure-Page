9a8bc3fed982b4422a38e4a8ec92bc639e9ec51b
BurakKontas <aburakkontas@trakya.edu.tr>
2024-02-17 17:39:44 +0300

    Remove unused code in Homepage component

--- Homepage.tsx
+++ Homepage.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import styles from './Homepage.module.scss'
 
 function Homepage() {
-  const navigate = useNavigate();
-
-  React.useEffect(() => {
-    navigate('/characters');
-  }, [])
 
   return (
     <div className={styles.container}>
